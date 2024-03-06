4af15e9a836fb9c59ffcf47f62cd43cd317735bb
oluwatoyosi oladipo <oladipooluwatoyosi@yahoo.com>
2024-03-06 16:06:32 +0100

    select option added to  NavForm

--- app/_components/NavForm.js
+++ app/_components/NavForm.js
@@ -8,7 +8,7 @@ function NavForm() {
   return (
     <form
       action=""
-      className={`flex grow rounded-md ${
+      className={`relative flex grow overflow-hidden rounded-md ${
         searchFocused ? "outline outline-4 outline-[#febd69]" : ""
       }`}
     >
@@ -16,9 +16,29 @@ function NavForm() {
         <div className="text-xs leading-8">All</div>
         <ArrowDownIcon className="text-xs" />
       </div>
+      <select
+        name=""
+        id=""
+        className="absolute left-0 top-1 h-9 cursor-pointer text-sm opacity-0"
+      >
+        <option selected value="">
+          All Departments
+        </option>
+        <option value=""> Arts &amp; Crafts</option>
+        <option value="">Automotive</option>
+        <option value="">Baby</option>
+        <option value="">Beauty & Personal care</option>
+        <option value="">Books</option>
+        <option value="">Boys&apos; Fashion</option>
+        <option value="">Computers</option>
+        <option value="">Deals</option>
+        <option value="">Digital Music</option>
+        <option value="">Electronics</option>
+        <option value="">Girls&apos; Fashion</option>
+      </select>
       <input
         type="text"
-        className="search-input h-10 grow pb-[0.65rem] pl-0 pt-2 indent-2 text-sm leading-none text-[#111] focus:outline-none"
+        className="search-input relative h-10 grow pb-[0.65rem] pl-0 pt-2 indent-2 text-sm leading-none text-[#111] focus:outline-none"
         placeholder="Search Amazon"
         onFocus={() => setSearchFocused(true)}
         onBlur={() => setSearchFocused(false)}
